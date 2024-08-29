f0235a4a9d9325a8f4e00e5d1feda2a8a41032d0
Cristiano <cristianosilvamolon@gmail.com>
2024-08-29 15:15:02 -0300

    criando a galeria II

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,81 @@
+import Section from '../Section'
+import { Items, Item, Action, Modal, ModalContent } from './styles'
+
+import zelda from '../../assets/images/zelda.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+
+import play from '../../assets/images/play.png'
+import zoom from '../../assets/images/zoom.png'
+import fechar from '../../assets/images/fechar.png'
+
+type GalleryItem = {
+  type: 'image' | 'video'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: zelda
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=BxpwcDn8D8tfFe_7'
+  }
+]
+
+type Props = {
+  defaultCover: string
+  name: string
+}
+
+const Gallery = ({ defaultCover, name }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  const getMediaIcon = (item: GalleryItem) => {
+    if (item.type === 'image') return zoom
+    return play
+  }
+
+  return (
+    <>
+      <Section title="Galeria" background="gray">
+        <Items>
+          {mock.map((media, index) => (
+            <Item key={media.url}>
+              <img
+                src={getMediaCover(media)}
+                alt={`Media ${index + 1} de ${name}`}
+              />
+              <Action>
+                <img
+                  src={getMediaIcon(media)}
+                  alt="Clique para maximizar a mídia"
+                />
+              </Action>
+            </Item>
+          ))}
+        </Items>
+      </Section>
+      <Modal>
+        <ModalContent className="container">
+          <header>
+            <h4>{name}</h4>
+            <img src={fechar} />
+          </header>
+          <img src={zelda} />
+        </ModalContent>
+        <div className="overlay"></div>
+      </Modal>
+    </>
+  )
+}
+
+export default Gallery
